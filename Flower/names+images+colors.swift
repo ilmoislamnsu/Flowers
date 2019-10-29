d13ca4f8e9e4b74ee4ef90fff1b539682669171a
Md Shafiqul Islam <ilmoislam@icloud.com>
2019-10-29 17:56:07 +0600

    DONE

---
+++ Flower/names+images+colors.swift
@@ -0,0 +1,29 @@
+//
+//  names+images+colors.swift
+//  Flower
+//
+//  Created by Shafiqul  Islam on 10/29/19.
+//  Copyright © 2019 Shafiqul  Islam. All rights reserved.
+//
+
+import UIKit
+
+//Flowers Array
+
+let waterlily = UIImage(named: "f0")!
+let pinklily = UIImage(named: "f1")!
+let redrose = UIImage(named: "f2")!
+let whiteflower = UIImage(named: "f3")!
+let violetFower = UIImage(named: "f4")!
+let multicolor = UIImage(named: "f5")!
+let joba = UIImage(named: "f6")!
+
+let imgArray = [waterlily, pinklily, redrose , whiteflower , violetFower , multicolor , joba]
+
+//NAMES
+
+let namesArray = ["LILY","PLILY","REDROSE","WHITE","VIOLET","MULTICOLOR","DARK"]
+
+// COLORS
+
+let colorsArray = [#colorLiteral(red: 0.8632009846, green: 0.8200251183, blue: 0.6387966273, alpha: 1),#colorLiteral(red: 0.8632009846, green: 0.6335081336, blue: 0.6387966273, alpha: 1),#colorLiteral(red: 0.2941176471, green: 0.5568627451, blue: 0.5529411765, alpha: 1),#colorLiteral(red: 0.2235294118, green: 0.3882352941, blue: 0.3843137255, alpha: 1),#colorLiteral(red: 0.1607843137, green: 0.2039215686, blue: 0.3843137255, alpha: 1),#colorLiteral(red: 0.2196078431, green: 0.05490196078, blue: 0.4980392157, alpha: 1),#colorLiteral(red: 0.3413437164, green: 0.05490196078, blue: 0.4980392157, alpha: 1)]
